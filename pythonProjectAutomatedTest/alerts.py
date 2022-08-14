66e259e56037239065f13a05f664f11c8acd7fb2
abdoulaye <acgueye@hotmail.fr>
2022-08-14 21:37:44 +0200

    test automtatise

---
+++ pythonProjectAutomatedTest/alerts.py
@@ -0,0 +1,21 @@
+import time
+
+from selenium import webdriver
+#Chrome driver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+service_obj = Service("C:/Program Files (x86)/chromedriver.exe")
+driver = webdriver.Chrome(service=service_obj)
+
+name = "Rahul"
+driver.get(" https://rahulshettyacademy.com/AutomationPractice/")
+driver.maximize_window()
+
+driver.find_element(By.CSS_SELECTOR, '#name').send_keys(name)
+driver.find_element(By.ID, 'alertbtn').click()
+
+alert = driver.switch_to.alert
+alertText = alert.text
+print(alertText)
+alert.accept()
--- pythonProjectAutomatedTest/findElementTest.py
+++ pythonProjectAutomatedTest/findElementTest.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
-service_obj = Service("C:/Users/Gueye/Documents/chromedriver.exe")
+service_obj = Service("C:/Program Files (x86)/chromedriver.exe")
 driver = webdriver.Chrome(service=service_obj)
 
 driver.get(" https://rahulshettyacademy.com/dropdownsPractise/")
